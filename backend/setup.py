0a5da8761c68379dde28e7fff688675638a8610f
Shreya Sharma <shreyas@audaxlabs.com>
2024-03-20 20:00:16 +0530

    Setup file added

---
+++ backend/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='DocuBot',
+    version='0.0.0',
+    author='Shreya Sharma',
+    author_email='qv.shreyasharma2001@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
